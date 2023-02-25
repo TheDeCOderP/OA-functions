0e4562980d07f2ddc63a128eb79cb51863fb44b6
TheDeCOderP <113170140+TheDeCOderP@users.noreply.github.com>
2023-02-26 03:41:47 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,354 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'HELLO_ELEMENTOR_VERSION', '2.6.1' );
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 800; // Pixels.
+}
+
+if ( ! function_exists( 'hello_elementor_setup' ) ) {
+	/**
+	 * Set up theme support.
+	 *
+	 * @return void
+	 */
+	function hello_elementor_setup() {
+		if ( is_admin() ) {
+			hello_maybe_update_theme_version_in_db();
+		}
+
+		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_load_textdomain', [ true ], '2.0', 'hello_elementor_load_textdomain' );
+		if ( apply_filters( 'hello_elementor_load_textdomain', $hook_result ) ) {
+			load_theme_textdomain( 'hello-elementor', get_template_directory() . '/languages' );
+		}
+
+		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_register_menus', [ true ], '2.0', 'hello_elementor_register_menus' );
+		if ( apply_filters( 'hello_elementor_register_menus', $hook_result ) ) {
+			register_nav_menus( [ 'menu-1' => __( 'Header', 'hello-elementor' ) ] );
+			register_nav_menus( [ 'menu-2' => __( 'Footer', 'hello-elementor' ) ] );
+		}
+
+		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_add_theme_support', [ true ], '2.0', 'hello_elementor_add_theme_support' );
+		if ( apply_filters( 'hello_elementor_add_theme_support', $hook_result ) ) {
+			add_theme_support( 'post-thumbnails' );
+			add_theme_support( 'automatic-feed-links' );
+			add_theme_support( 'title-tag' );
+			add_theme_support(
+				'html5',
+				[
+					'search-form',
+					'comment-form',
+					'comment-list',
+					'gallery',
+					'caption',
+					'script',
+					'style',
+				]
+			);
+			add_theme_support(
+				'custom-logo',
+				[
+					'height'      => 100,
+					'width'       => 350,
+					'flex-height' => true,
+					'flex-width'  => true,
+				]
+			);
+
+			/*
+			 * Editor Style.
+			 */
+			add_editor_style( 'classic-editor.css' );
+
+			/*
+			 * Gutenberg wide images.
+			 */
+			add_theme_support( 'align-wide' );
+
+			/*
+			 * WooCommerce.
+			 */
+			$hook_result = apply_filters_deprecated( 'elementor_hello_theme_add_woocommerce_support', [ true ], '2.0', 'hello_elementor_add_woocommerce_support' );
+			if ( apply_filters( 'hello_elementor_add_woocommerce_support', $hook_result ) ) {
+				// WooCommerce in general.
+				add_theme_support( 'woocommerce' );
+				// Enabling WooCommerce product gallery features (are off by default since WC 3.0.0).
+				// zoom.
+				add_theme_support( 'wc-product-gallery-zoom' );
+				// lightbox.
+				add_theme_support( 'wc-product-gallery-lightbox' );
+				// swipe.
+				add_theme_support( 'wc-product-gallery-slider' );
+			}
+		}
+	}
+}
+add_action( 'after_setup_theme', 'hello_elementor_setup' );
+
+function hello_maybe_update_theme_version_in_db() {
+	$theme_version_option_name = 'hello_theme_version';
+	// The theme version saved in the database.
+	$hello_theme_db_version = get_option( $theme_version_option_name );
+
+	// If the 'hello_theme_version' option does not exist in the DB, or the version needs to be updated, do the update.
+	if ( ! $hello_theme_db_version || version_compare( $hello_theme_db_version, HELLO_ELEMENTOR_VERSION, '<' ) ) {
+		update_option( $theme_version_option_name, HELLO_ELEMENTOR_VERSION );
+	}
+}
+
+if ( ! function_exists( 'hello_elementor_scripts_styles' ) ) {
+	/**
+	 * Theme Scripts & Styles.
+	 *
+	 * @return void
+	 */
+	function hello_elementor_scripts_styles() {
+		$enqueue_basic_style = apply_filters_deprecated( 'elementor_hello_theme_enqueue_style', [ true ], '2.0', 'hello_elementor_enqueue_style' );
+		$min_suffix          = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		if ( apply_filters( 'hello_elementor_enqueue_style', $enqueue_basic_style ) ) {
+			wp_enqueue_style(
+				'hello-elementor',
+				get_template_directory_uri() . '/style' . $min_suffix . '.css',
+				[],
+				HELLO_ELEMENTOR_VERSION
+			);
+		}
+
+		if ( apply_filters( 'hello_elementor_enqueue_theme_style', true ) ) {
+			wp_enqueue_style(
+				'hello-elementor-theme-style',
+				get_template_directory_uri() . '/theme' . $min_suffix . '.css',
+				[],
+				HELLO_ELEMENTOR_VERSION
+			);
+		}
+	}
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_scripts_styles' );
+
+if ( ! function_exists( 'hello_elementor_register_elementor_locations' ) ) {
+	/**
+	 * Register Elementor Locations.
+	 *
+	 * @param ElementorPro\Modules\ThemeBuilder\Classes\Locations_Manager $elementor_theme_manager theme manager.
+	 *
+	 * @return void
+	 */
+	function hello_elementor_register_elementor_locations( $elementor_theme_manager ) {
+		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_register_elementor_locations', [ true ], '2.0', 'hello_elementor_register_elementor_locations' );
+		if ( apply_filters( 'hello_elementor_register_elementor_locations', $hook_result ) ) {
+			$elementor_theme_manager->register_all_core_location();
+		}
+	}
+}
+add_action( 'elementor/theme/register_locations', 'hello_elementor_register_elementor_locations' );
+
+if ( ! function_exists( 'hello_elementor_content_width' ) ) {
+	/**
+	 * Set default content width.
+	 *
+	 * @return void
+	 */
+	function hello_elementor_content_width() {
+		$GLOBALS['content_width'] = apply_filters( 'hello_elementor_content_width', 800 );
+	}
+}
+add_action( 'after_setup_theme', 'hello_elementor_content_width', 0 );
+
+if ( is_admin() ) {
+	require get_template_directory() . '/includes/admin-functions.php';
+}
+
+/**
+ * If Elementor is installed and active, we can load the Elementor-specific Settings & Features
+*/
+
+// Allow active/inactive via the Experiments
+require get_template_directory() . '/includes/elementor-functions.php';
+
+/**
+ * Include customizer registration functions
+*/
+function hello_register_customizer_functions() {
+	if ( is_customize_preview() ) {
+		require get_template_directory() . '/includes/customizer-functions.php';
+	}
+}
+add_action( 'init', 'hello_register_customizer_functions' );
+
+if ( ! function_exists( 'hello_elementor_check_hide_title' ) ) {
+	/**
+	 * Check hide title.
+	 *
+	 * @param bool $val default value.
+	 *
+	 * @return bool
+	 */
+	function hello_elementor_check_hide_title( $val ) {
+		if ( defined( 'ELEMENTOR_VERSION' ) ) {
+			$current_doc = Elementor\Plugin::instance()->documents->get( get_the_ID() );
+			if ( $current_doc && 'yes' === $current_doc->get_settings( 'hide_title' ) ) {
+				$val = false;
+			}
+		}
+		return $val;
+	}
+}
+add_filter( 'hello_elementor_page_title', 'hello_elementor_check_hide_title' );
+
+/**
+ * Wrapper function to deal with backwards compatibility.
+ */
+if ( ! function_exists( 'hello_elementor_body_open' ) ) {
+	function hello_elementor_body_open() {
+		if ( function_exists( 'wp_body_open' ) ) {
+			wp_body_open();
+		} else {
+			do_action( 'wp_body_open' );
+		}
+	}
+}
+
+
+// custom data
+
+
+
+add_shortcode ('woo_cart_but', 'woo_cart_but' );
+/**
+ * Create Shortcode for WooCommerce Cart Menu Item
+ */
+function woo_cart_but() {
+	ob_start();
+ 
+  $cart_count = WC()->cart->cart_contents_count;   // Set variable for cart item count
+        $cart_url = wc_get_cart_url();  // Set Cart URL
+  
+        ?>
+         
+			<a class="menu-item cart-contents" href="<?php echo $cart_url; ?>" title="My Basket">
+	    <?php
+        if ( $cart_count > 0 ) {
+       ?>
+            <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+        <?php
+        }
+        ?>
+        </a>
+
+        <?php
+	        
+    return ob_get_clean();
+ 
+}
+
+add_filter( 'woocommerce_add_to_cart_fragments', 'woo_cart_but_count' );
+/**
+ * Add AJAX Shortcode when cart contents update
+ */
+function woo_cart_but_count( $fragments ) {
+ 
+    ob_start();
+    
+    $cart_count = WC()->cart->cart_contents_count;
+    $cart_url = wc_get_cart_url();
+    
+    ?>
+    <a class="cart-contents menu-item" href="<?php echo $cart_url; ?>" title="<?php _e( 'View your shopping cart' ); ?>">
+	<?php
+    if ( $cart_count > 0 ) {
+        ?>
+        <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+        <?php            
+    }
+        ?></a>
+    <?php
+ 
+    $fragments['a.cart-contents'] = ob_get_clean();
+     
+    return $fragments;
+}
+
+
+add_filter('wp_nav_menu_items', 'do_shortcode');
+
+
+add_filter( 'dgwt/wcas/form/magnifier_ico', function ( $html, $class ) {
+  $html = '<i class="fa fa-search ' . $class . '"></i>';
+  return $html;
+}, 10, 2 );
+
+// invoice 
+
+
+
+function show_all_products_on_shop_page( $query ) {
+    if ( is_shop() && $query->is_main_query() ) {
+        $query->set( 'posts_per_page', -1 );
+    }
+}
+add_action( 'pre_get_posts', 'show_all_products_on_shop_page' );
+
+
+
+function add_discount_to_cart( $cart ) {
+    if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
+        return;
+    }
+
+    // Calculate the discount amount
+    $discount = $cart->get_subtotal() * 0.1;
+
+    // Add the discount as a fee
+$cart->add_fee( __('Discount', 'woocommerce'), -$discount );
+}
+add_action( 'woocommerce_cart_calculate_fees', 'add_discount_to_cart' );
+
+
+
+;// Add review count on top of the title
+function add_review_count_on_product_title() {
+    global $product;
+    $rating_count = $product->get_rating_count();
+    if ( $rating_count > 0 ) {
+        echo '<div class="woocommerce-product-rating">';
+        echo '<span class="review-count">(' . $rating_count  . '</span>';
+        echo ' Customer Reviews )';
+        echo '</div>';
+    }
+}
+add_action( 'woocommerce_after_shop_loop_item_title', 'add_review_count_on_product_title', 5 );
+
+
+
+add_action( 'woocommerce_cart_calculate_fees', 'custom_cod_fee' );
+function custom_cod_fee() {
+    // Only add fee if cash on delivery is selected
+    if ( ! isset( WC()->session->chosen_payment_method ) || 'cod' !== WC()->session->chosen_payment_method ) {
+        return;
+    }
+
+    // Get cart total
+    $cart_total = WC()->cart->subtotal;
+
+    // Add fee based on cart total
+    if ( $cart_total < 2000 ) {
+        $fee = 40;
+    } else {
+        $fee = 60;
+    }
+
+    // Add fee
+    WC()->cart->add_fee( 'COD Charges', $fee, true, 'standard' );
+}
+
